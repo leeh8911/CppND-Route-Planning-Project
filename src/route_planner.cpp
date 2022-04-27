8e631e59e63d1f937dfaf97d86ef8b9f0b2ae9b0
sangwon <leeh8911@gmail.com>
2022-04-27 21:18:42 +0000

    minor revision about TODO 5

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -48,7 +48,11 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 // - Return the pointer.
 
 RouteModel::Node *RoutePlanner::NextNode() {
-    std::sort(open_list.begin(), open_list.end(), [](auto &e1, auto &e2){return (e1->g_value + e1->h_value) < (e2->g_value + e2->h_value);});
+    std::sort(open_list.begin(), open_list.end(), [](auto &e1, auto &e2){return (e1->g_value + e1->h_value) > (e2->g_value + e2->h_value);});
+    RouteModel::Node* node = open_list.back();
+    open_list.pop_back();
+
+    return node;
 }
 
 
